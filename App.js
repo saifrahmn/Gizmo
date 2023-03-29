55a3f1d91af7ce4b1abbb9166f5f560ab4a9406c
saifrahmn <saifrahmn78@gmail.com>
2023-03-29 16:20:33 +0530

    update

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from "react-native-login-screen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> These is akola</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0B2447',
     alignItems: 'center',
     justifyContent: 'center',
   },
